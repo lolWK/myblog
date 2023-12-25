0ed29ab3dcf7c4217bfb39af347b65b02e8c6086
WK <mwk0725@gmail.com>
2023-12-26 00:42:48 +0900

    chore: font, icon svg 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-import Tiptap from '@/components/Tiptap';
-import Image from 'next/image';
+import LeftIcon from '@/assets/icons/icon-arrow-left.svg';
+import ClockIcon from '@/assets/icons/icon-clock.svg';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      HomePage
-    </main>
+    <p>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 font-p font-medium">
+        HomePage
+        <LeftIcon
+          width={100}
+          height={100}
+          fill="#8782D4"
+          stroke="#8782D4"
+          color="#8782D4"
+        />
+        <ClockIcon
+          width={30}
+          height={30}
+          fill="#D4B382"
+          stroke="#D4B382"
+          color="#D4B382"
+        />
+      </main>
+    </p>
   );
 }
