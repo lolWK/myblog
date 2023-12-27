084a257c9d91cc946c10b805df7f41d25b2db7e1
WK <mwk0725@gmail.com>
2023-12-28 01:02:30 +0900

    feat: 다크 모드 토글 구현

---
+++ src/components/ModeToggle.tsx
@@ -0,0 +1,29 @@
+import { useTheme } from 'next-themes';
+import { Button } from '@/components/ui/button';
+import MoonIcon from '@/assets/icons/icon-moon.svg';
+import SunIcon from '@/assets/icons/icon-sun.svg';
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    if (theme === 'dark') {
+      setTheme('light');
+    } else {
+      setTheme('dark');
+    }
+  };
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      className="hover:bg-transparent"
+      onClick={toggleTheme}
+    >
+      <SunIcon className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
