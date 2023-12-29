9cbfb8cab905409e6f58a4f223d50710356f9a8c
WK <mwk0725@gmail.com>
2023-12-30 05:17:14 +0900

    fix: shadcn Label로 교체

--- src/components/input/SearchInput.tsx
+++ src/components/input/SearchInput.tsx
@@ -1,16 +1,18 @@
 import { Input } from '@/components/ui/input';
 import SearchIcon from '@/assets/icons/icon-search.svg';
+import { Label } from '@/components/ui/label';
 
 export default function SearchInput() {
   return (
     <form className="relative">
-      <label
+      <Label
         htmlFor="search"
         className="colors-foreground cursor-pointer absolute top-[50%] -translate-y-2/4 right-3"
       >
         <SearchIcon width={16} height={16} stroke="currentColor" />
         <span className="sr-only">검색</span>
-      </label>
+      </Label>
+
       <Input
         type="text"
         className="absolute -top-[20px] right-0 bg-transparent w-0 focus:w-[160px] border-none focus:outline-none transition-width duration-300 pr-8"
