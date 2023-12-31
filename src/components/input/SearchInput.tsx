ac96d53755b93291db6bae220fe66f53fc2a1139
WK <mwk0725@gmail.com>
2023-12-31 23:50:35 +0900

    fix: 검색 input id 겹침 오류 수정

--- src/components/input/SearchInput.tsx
+++ src/components/input/SearchInput.tsx
@@ -1,12 +1,22 @@
+'use client';
+
+import { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import SearchIcon from '@/assets/icons/icon-search.svg';
 import { Label } from '@/components/ui/label';
 
+let uniqueIdCounter = 0;
+
 export default function SearchInput() {
+  const [id] = useState(() => {
+    uniqueIdCounter += 1;
+    return `search-${uniqueIdCounter}`;
+  });
+
   return (
     <form className="relative">
       <Label
-        htmlFor="search"
+        htmlFor={id}
         className="colors-foreground cursor-pointer absolute top-[50%] -translate-y-2/4 right-3"
       >
         <SearchIcon width={16} height={16} stroke="currentColor" />
@@ -16,7 +26,7 @@ export default function SearchInput() {
       <Input
         type="text"
         className="absolute -top-[20px] right-0 bg-transparent w-0 focus:w-[160px] border-none focus:outline-none transition-width duration-300 pr-8"
-        id="search"
+        id={id}
         autoComplete="off"
       />
     </form>
