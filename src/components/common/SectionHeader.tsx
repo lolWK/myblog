0032843e1f2de69dde8ee83689a2b4144b1e5c19
ᴡᴏɴᴋʏᴜɴɢ ᴍɪɴ <mwk0725@gmail.com>
2024-01-02 01:41:02 +0900

    페이지 SectionHeader 컴포넌트 추가 (#17)
    * fix: type 생성 script 오타 수정
    
    * refactor: 로그인 상태 관리 Context 추가 및 리팩토링
    
    * feat: 메인 페이지 SectionHeader 컴포넌트 구현
    
    * feat: PAGE_INFO 상수 추가

---
+++ src/components/common/SectionHeader.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { PAGE_INFO } from '@/constants/pageInfo';
+import { SessionContext } from '@/contexts/SessionProvider';
+import { useContext } from 'react';
+
+type Props = {
+  pageType: 'blog' | 'notes' | 'archive';
+};
+
+export default function SectionHeader({ pageType }: Props) {
+  const session = useContext(SessionContext);
+  const { name, description } = PAGE_INFO[pageType];
+
+  return (
+    <section className="mt-[80px] flex justify-between">
+      <div className="font-h text-px16-300 flex items-center gap-8">
+        <h2>{name}</h2>
+        <p className="sm-visible-hide">{description}</p>
+      </div>
+
+      {session && (
+        /* TODO. 글쓰기 페이지로 이동 */
+        <Button
+          variant="secondary"
+          className="font-p text-px14-300 text-foreground px-4 py-2"
+        >
+          글쓰기
+        </Button>
+      )}
+    </section>
+  );
+}
---
+++ src/constants/pageInfo.ts
@@ -0,0 +1,14 @@
+export const PAGE_INFO = {
+  blog: {
+    name: 'Blog',
+    description: '지식, 경험, 생각을 공유하기 위한 공간',
+  },
+  notes: {
+    name: 'Notes',
+    description: '개인 기록 공간',
+  },
+  archive: {
+    name: 'Archive',
+    description: ' 모든 기록 정리 공간',
+  },
+};
