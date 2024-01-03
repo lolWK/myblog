54207bda42fb6bbcb4a6612a14c63ac1a22680d6
WK <mwk0725@gmail.com>
2024-01-04 04:49:30 +0900

    feat: 메인 페이지("/") -> ("blog")로 리다이렉트 되게 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,3 @@
-// import LeftIcon from '@/assets/icons/icon-arrow-left.svg';
-
-import SectionHeader from '@/components/common/SectionHeader';
-
-// import ClockIcon from '@/assets/icons/icon-clock.svg';
-
-export default function Home() {
-  return (
-    <main>
-      <SectionHeader pageType="blog" />
-      {/* <main className="flex min-h-screen flex-col items-center justify-between p-24 font-p font-medium">
-        HomePage
-        <LeftIcon
-          width={100}
-          height={100}
-          fill="#8782D4"
-          stroke="#8782D4"
-          color="#8782D4"
-        />
-        <ClockIcon
-          width={30}
-          height={30}
-          fill="#D4B382"
-          stroke="#D4B382"
-          color="#D4B382"
-        />
-      </main> */}
-    </main>
-  );
+export default async function Home() {
+  return <></>;
 }
