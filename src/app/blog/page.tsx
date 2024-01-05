f9216351abf0ca2c539bf35e4ff5a73236598161
WK <mwk0725@gmail.com>
2024-01-06 02:50:44 +0900

    feat: PostList와 데이터와 연동, UI 수정 및 추가

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,22 @@
 import SectionHeader from '@/components/common/SectionHeader';
 import PostList from '@/components/post/PostList';
-import { fetchPosts } from '@/queries/post';
+import PostListHeader from '@/components/post/PostListHeader';
+import { fetchPostCountByType, fetchPosts } from '@/queries/post';
 
-export default function BlogPage() {
-  // const posts = await fetchPosts('blog', 0);
+export default async function BlogPage() {
+  const posts = await fetchPosts('blog', 0);
+  const postCount = await fetchPostCountByType('blog');
 
   // console.log(posts);
+  console.log(postCount);
   return (
-    <main>
+    <main className="flex flex-col gap-10">
       <SectionHeader pageType="blog" />
-
-      {/* <PostList posts={posts} /> */}
+      {/* 전체 글 갯수 따로 가져와야하네? 오호 */}
+      <div>
+        <PostListHeader leftText="All" rightText={11} />
+        <PostList posts={posts} />
+      </div>
     </main>
   );
 }
