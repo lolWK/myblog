3b46c7c596238b4e3e9ef284f31e2b241712cc71
WK <mwk0725@gmail.com>
2024-01-12 00:37:56 +0900

    refactor: 자잘한 css 약간 수정

--- src/components/common/CustomBadge.tsx
+++ src/components/common/CustomBadge.tsx
@@ -10,8 +10,8 @@ export default function CustomBadge({ type, children }: CustomBadgeProps) {
     <Badge
       className={`${
         type === 'topic'
-          ? 'w-[64px] justify-center rounded py-1 font-h text-px12-400 text-background'
-          : 'font-p text-px12-500 font-medium'
+          ? 'w-16 justify-center rounded py-1 font-h font-normal text-background'
+          : 'font-p font-medium'
       }`}
       variant={`${type === 'topic' ? 'default' : 'secondary'}`}
     >
