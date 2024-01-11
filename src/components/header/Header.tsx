6e755834368d20511983366be956a5a3fe144918
WK <mwk0725@gmail.com>
2024-01-12 00:37:56 +0900

    refactor: 자잘한 css 약간 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,22 +3,25 @@ import ThemeModeToggle from '@/components/ThemeModeToggle';
 import SearchInput from '@/components/input/SearchInput';
 import MainHeaderNav from '@/components/navigation/MainHeaderNav';
 import MainHeaderNavM from '@/components/navigation/MainHeaderNavM';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between h-20">
-      <h1 className="colors-foreground">
-        <Logo fill="currentColor" />
-        <span className="sr-only">1klog.dev blog</span>
+    <header className='flex h-20 items-center justify-between'>
+      <h1 className='colors-foreground'>
+        <Link href='/blog'>
+          <Logo fill='currentColor' />
+          <span className='sr-only'>1klog.dev blog</span>
+        </Link>
       </h1>
 
-      <div className="hidden md:flex items-center">
+      <div className='hidden items-center md:flex'>
         <SearchInput />
         <MainHeaderNav />
         <ThemeModeToggle />
       </div>
 
-      <div className="md:hidden flex items-center">
+      <div className='flex items-center md:hidden'>
         <SearchInput />
         <ThemeModeToggle />
         <MainHeaderNavM />
