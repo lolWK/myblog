55c00c43c8395046f92f515e54b87e004fb8a2ef
WK <mwk0725@gmail.com>
2024-01-16 02:18:01 +0900

    refactor: postCount 적용

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,18 +4,17 @@ import PostList from '@/components/post/PostList';
 import PostListHeader from '@/components/post/PostListHeader';
 import { fetchPostCountByType, fetchPosts } from '@/queries/post';
 
+export const revalidate = 0;
+
 export default async function BlogPage() {
   const posts = await fetchPosts('blog', 0);
   const postCount = await fetchPostCountByType('blog');
 
-  // console.log(posts);
-  console.log(postCount);
   return (
     <main className='flex flex-col gap-10'>
       <SectionHeader pageType='blog' />
-      {/* 전체 글 갯수 따로 가져와야하네? 오호 */}
       <div>
-        <PostListHeader leftText='All' rightText={11} />
+        <PostListHeader leftText='All' rightText={postCount} />
         <PostList posts={posts} />
         <PagePagination />
       </div>
