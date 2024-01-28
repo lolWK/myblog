6d484f84810875f7b874e9f631457f7238c05167
WK <mwk0725@gmail.com>
2024-01-28 21:13:25 +0900

    feat: 필터 목록 선택된 항목 스타일 추가 및 자잘자잘 수정

--- src/components/post/PostListHeader.tsx
+++ src/components/post/PostListHeader.tsx
@@ -2,17 +2,19 @@ import CustomBadge from '@/components/common/CustomBadge';
 
 interface PostListHeaderProps {
   leftText: string;
-  rightText: number;
+  rightText?: number | null;
 }
 
 export default function PostListHeader({
   leftText,
-  rightText,
+  rightText = null,
 }: PostListHeaderProps) {
   return (
     <div className='flex w-full justify-between border-b border-foreground pb-4'>
       <p className='font-h text-px14-300'>{leftText}</p>
-      <CustomBadge type='number'>{rightText}</CustomBadge>
+      {typeof rightText === 'number' && (
+        <CustomBadge type='number'>{rightText}</CustomBadge>
+      )}
     </div>
   );
 }
